844311fce66feea219b96b07aa8ec55abf55d24e
mr-alexmartyn <111350218+mr-alexmartyn@users.noreply.github.com>
2023-01-31 16:08:07 +0200

    task 2 complete

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,20 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCAL_KEY = 'videoplayer-current-time';
+
+player.on('timeupdate', throttle(onPlay, 1000));
+function onPlay(data) {
+  localStorage.setItem(LOCAL_KEY, data.seconds);
+}
+
+function updateTime() {
+  const currentTime = localStorage.getItem(LOCAL_KEY);
+  if (currentTime) {
+    player.setCurrentTime(currentTime);
+  }
+}
+updateTime();
